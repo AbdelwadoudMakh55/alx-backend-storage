a3edb29f9c9075183470c1c2521d6a40bfe26bb5
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-24 17:12:16 +0000

    Calculate lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Computing lifespan of metal bands
 SELECT band_name, IF(split is NOT NULL, split, 2022) - formed AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
