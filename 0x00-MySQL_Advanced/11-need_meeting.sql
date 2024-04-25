1a8492edb5c3f4480a2201155cd95ecf9190df3b
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-25 10:39:14 +0000

    Create VIEW

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,4 @@
 CREATE VIEW need_meeting AS
 SELECT name 
 FROM students
-WHERE score < 80 AND (last_meeting IS NULL OR MONTH(CURDATE()) - MONTH(last_meeting) > 1);
-ORDER BY score;
+WHERE score < 80 AND (last_meeting IS NULL OR MONTH(CURDATE()) - MONTH(last_meeting) > 1 OR YEAR(CURDATE()) > YEAR(last_meeting));
