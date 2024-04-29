ff0487f3b47ebd54bebb17c3f41759a631a1e061
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-29 23:45:30 +0200

    Compute average and sort

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+sort students by avg score
+"""
+
+
+def top_students(mongo_collection):
+    """ Return students sorted by avg score """
+    students = list(mongo_collection.find({}))
+    for student in students:
+        if len(student.get('topics')) > 0:
+            avg = sum([topic.get('score') for topic in student.get('topics')]) \
+                  / len(student.get('topics'))
+            value = {'$set': {'averageScore': avg}}
+            mongo_collection.update({"id_": student.get('id_')}, value)
+    return mongo_collection.find().sort({"averageScore": -1})
+
