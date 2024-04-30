917a84efd971c200a45894aa65e837888901329e
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-30 19:41:47 +0200

    Display infos about function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -61,7 +61,7 @@ def replay(fn: Callable) -> None:
     cache = fn.__self__
     inputs = cache._redis.lrange("{}:inputs".format(fn.__qualname__), 0, -1)
     outputs = cache._redis.lrange("{}:outputs".format(fn.__qualname__), 0, -1)
-    num_calls = cache.get_int(fn.__qualname__)
+    num_calls = cache._redis.get(fn.__qualname__)
     print(f'{fn.__qualname__} was called {num_calls.decode("utf-8")} times:')
     for pair in tuple(zip(inputs, outputs)):
         print(f'{fn.__qualname__}(*{pair[0].decode("utf-8")}) -> \
